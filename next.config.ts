e500c664acaa8e973ec618578a6319d61364599e
Gabriel Costa Luiz <gabrielbragacostaluiz@gmail.com>
2025-01-13 13:03:32 +0000

    fix: added utfs hostname and async header * origin and fixed page create itineraries

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  //headers
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
@@ -20,6 +43,9 @@ const nextConfig: NextConfig = {
       {
         hostname: "pinata-media.mypinata.cloud",
       },
+      {
+        hostname: "utfs.io"
+      }
       
     ],
   },
